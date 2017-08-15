60df9248a8fe4e6bfc3e18943c3ab7e5d1509686
ZiimpLe <ZiimpLe@foxmail.com>
2017-08-15 13:06:53 +0800

    add js study demo

--- js_study/lesson_01/lesson_01.js
+++ js_study/lesson_01/lesson_01.js
@@ -1,17 +1,3 @@
 /**
  * Created by ziimple on 2017/7/1.
  */
-window.onload = function () {
-    var btn = document.getElementById("btn");
-    var p = document.getElementById("p");
-    btn.onclick = function changeInner() {
-        var str = document.getElementById("p").innerHTML;
-        p.innerHTML = str.search(/w3cschool/i);
-    };
-
-    function closeConfirm() {
-        confirm("是否要关闭页面？");
-    }
-
-    document.getElementsByTagName("body")[0].onunload = closeConfirm();
-};
